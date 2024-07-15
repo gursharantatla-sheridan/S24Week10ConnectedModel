552b7f2ebb5bc1ab629224d7c5ae726dda1982e9
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-15 17:04:08 -0400

    Implement load

--- S24Week10ConnectedModel/MainWindow.xaml.cs
+++ S24Week10ConnectedModel/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace S24Week10ConnectedModel
 {
@@ -21,9 +23,45 @@ namespace S24Week10ConnectedModel
             InitializeComponent();
         }
 
+        private void LoadGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(Data.ConnectionStr))
+            {
+                string query = "select EmployeeID, FirstName, LastName, City from Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
+            //conn.Close();
+        }
+
         private void btnLoad_Click(object sender, RoutedEventArgs e)
         {
+            LoadGrid();
+        }
 
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Data.ConnectionStr))
+            {
+                // string concatenation - DO NOT USE IT
+                string query = "select EmployeeID, FirstName, LastName, City from Employees where FirstName='" + txtFirstname.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
         }
     }
 }
